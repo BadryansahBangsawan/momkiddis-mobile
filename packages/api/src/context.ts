84413f73316ae68fbdc108cb4b56cdad4f9296b9
Badryansah Bangsawan <badryansah99@gmail.com>
2026-06-05 01:18:32 +0800

    initial commit

---
+++ packages/api/src/context.ts
@@ -0,0 +1,13 @@
+import { createAuth } from "@momkiddis/auth";
+
+export async function createContext({ req }: { req: Request }) {
+  const session = await createAuth().api.getSession({
+    headers: req.headers,
+  });
+  return {
+    auth: null,
+    session,
+  };
+}
+
+export type Context = Awaited<ReturnType<typeof createContext>>;
